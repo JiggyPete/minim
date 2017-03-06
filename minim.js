e3707d315a5b71178d34aed61a89eb7b9253891c
Peter Aitken <jiggypete@Peters-MacBook-Pro.local>
2017-03-06 02:58:13 +0000

    Button presses in-browser update minim

--- minim.js
+++ minim.js
@@ -15,6 +15,8 @@ function onMIDISuccess(midiAccess) {
   for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
     input.value.onmidimessage = minimInput;
   }
+
+  setupBrowserToController(midi.outputs);
 }
 
 function onMIDIFailure(error) {
@@ -23,17 +25,49 @@ function onMIDIFailure(error) {
 
 function minimInput(message) {
   console.log(message.data)
-  if(message.data[0] == 176) {
-    document.getElementById("minim__slider").textContent = message.data[2]
+
+  updateBrowserPads(message.data);
+}
+
+function updateBrowserPads(midiData) {
+  if(midiData[0] == 176) {
+    document.getElementById("minim__slider").textContent = midiData[2]
     return
   }
 
-  var element = document.getElementById(message.data[1])
+  var element = document.getElementById(midiData[1])
   if(element != null) {
-    element.style.backgroundColor = colorFor(message.data[2]);
+    element.style.backgroundColor = colorFor(midiData[2]);
   }
 }
 
 function colorFor(number) {
   return number == 0 ? "white" : "orange";
+}
+
+function setupBrowserToController(outputs) {
+  outputs.forEach( function(output) {
+    output.open();
+    listenForEventOn(output, allBrowserPads(), "mousedown", 127);
+    listenForEventOn(output, allBrowserPads(), "mouseup", 0);
+  })
+}
+
+function allBrowserPads() {
+  return [2,3,4,5,6,    32, 33, 34, 35, 36, 37, 38, 39 ,40, 41, 42, 43, 44, 45, 46, 47].map( browserPad );
+}
+
+function browserPad(id) {
+  return document.getElementById(id)
+}
+
+function listenForEventOn(output, elements, eventName, padVelocity) {
+  elements.forEach( function(element) {
+    element.addEventListener(eventName, function(event) {
+      var padId = event.srcElement.id;
+      var midiData = [144, padId, padVelocity];
+      output.send(midiData);
+      updateBrowserPads(midiData);
+    })
+  })
 }
